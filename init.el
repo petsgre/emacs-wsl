69d373a333fd811c3eba2446379172f73d563739
root <root@purang879.purang.com>
2024-02-05 14:31:43 +0800

    init project

---
+++ init.el
@@ -0,0 +1,36 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+;; 使用use-package下载一些包
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; 添加redo undo 树
+(use-package undo-tree
+  :ensure t
+  :init
+    (global-undo-tree-mode))
+
+(global-set-key (kbd "C-c SPC")   'set-mark-command)
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+(global-set-key (kbd"C-x C-j") 'dired-jump)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
