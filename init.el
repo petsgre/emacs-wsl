990aa177ec05fc2b1c5398369651fa30f06a96a9
root <root@purang879.purang.com>
2024-02-06 15:25:08 +0800

    update many

--- init.el
+++ init.el
@@ -7,18 +7,98 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+;; ivy 结合增强
+(use-package ivy
+  :ensure t
+  :diminish ivy-mode
+    :hook (after-init . ivy-mode))
+(electric-pair-mode 1)
+
+(use-package fzf
+  :ensure t)
+
+;; (use-package typescript-mode
+;;   :config
+;;   (define-derived-mode typescriptreact-mode typescript-mode
+;;     "TypeScript TSX"))
+
+
+(use-package swiper
+  :ensure t
+  :bind
+  ("C-r" . swiper))
+
+(use-package yasnippet
+  :ensure
+  :init
+  (yas-global-mode 1))
+
+;; (add-to-list 'load-path "../lsp-bridge")
+;; (require 'yasnippet)
+;; (yas-global-mode 1)
+;; (require 'lsp-bridge)
+;; (global-lsp-bridge-mode)
+
+(use-package web-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode)))
+
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+	      ("s-p" . projectile-command-map)
+	      ("C-c p" . projectile-command-map)))
 
 ;; 添加redo undo 树
 (use-package undo-tree
   :ensure t
   :init
-    (global-undo-tree-mode))
+  (global-undo-tree-mode))
+
+(use-package linum
+  :init
+  (progn
+    (global-linum-mode t)
+    (setq linum-format "%4d  ")
+    (set-face-background 'linum nil)
+        ))
+
+(use-package try
+  :ensure t)
 ;; magit
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)
-	          ("C-x C-g" . magit-status)))
+	 ("C-x C-g" . magit-status)))
+;; which key
+(use-package which-key
+  :ensure t
+  :config
+    (which-key-mode))
 
+;; auto-complete
+(use-package auto-complete
+  :ensure t
+  :init
+  (progn
+    (ac-config-default)
+    (global-auto-complete-mode t)
+    ))
+
+;; lsp mode
+(use-package lsp-mode
+  :ensure t)
+
+
+;; (use-package typescript-mode
+;;   :ensure t
+;;   :config
+;;   (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode)))
+
+;; wsl下选中
 (global-set-key (kbd "C-c SPC")   'set-mark-command)
 
 (recentf-mode 1)
